390c3665d8708e76ccc732431571e5b59ee48fca
BalasaiAnish <ponnaluribalasai@gmail.com>
2025-06-11 20:46:13 +0530

    initial commit

---
+++ cuda/src/fourier.cu
@@ -0,0 +1,10 @@
+#include <__clang_cuda_math.h>
+#include <cuda.h>
+
+__global__ void fourierLayer(float **x, float **freqs, float *xout){
+
+}
+
+__global__ void fourierFilter(float **x, float **freqs, float *xout){
+
+}
